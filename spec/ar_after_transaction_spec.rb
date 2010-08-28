8b451b4eb24f891051794848147a7b70cc873e5c
grosser <grosser.michael@gmail.com>
2010-08-28 13:52:26 +0200

    gather requirements

--- spec/ar_after_transaction_spec.rb
+++ spec/ar_after_transaction_spec.rb
@@ -4,4 +4,16 @@ describe ARAfterTransaction do
   it "has a VERSION" do
     ARAfterTransaction::VERSION.should =~ /^\d+\.\d+\.\d+$/
   end
+
+  it "executes after a transaction"
+
+  it "does not execute when transaction was rolled back"
+
+  it "executes when no transaction is open"
+
+  it "executes when open transactions are normal"
+
+  it "does not execute the same callback twice when successful"
+
+  it "does not execute the same callback twice when failed"
 end
