52564ea6fafce1474135165666c58fae578103c5
grosser <grosser.michael@gmail.com>
2012-01-08 17:58:54 -0800

    fix specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,4 @@ current = File.dirname(File.dirname(__FILE__))
 $LOAD_PATH << "#{current}/lib"
 require "#{current}/init"
 require "#{current}/spec/setup_database"
+require "ar_after_transaction/version"
